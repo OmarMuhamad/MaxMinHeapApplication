5d5056c5e93c880b46df26108f79a641931dff99
Omar Mohammad <omarmohamed75689@gmail.com>
2023-05-15 00:51:00 +0300

    Header updated

--- Header.h
+++ Header.h
@@ -1,12 +1,7 @@
 // Author: OMohammad
 // Purpose: Store info in Min/Max Heap
 
-/* MAIN ALOG
-* 1 - first we will open the file and get the information from it
-* 2 - we will create an array of Student (of objects)
-* 3 - we will then sort the array of Student based on the GPA in the array
-* 4 - if we add a student we will create object and add to the array
-*/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
